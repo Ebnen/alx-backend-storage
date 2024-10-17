71a7876bd5421070957815b235c7bd667c70cc85
Ebnen <Ebenzy1996@gmail.com>
2024-10-17 13:00:42 +0100

    sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,5 @@
 -- This script creates a table called users with the following columns:
 -- id: an integer that auto-increments and is the primary key
--- email: a string that cannot be null and must be unique
--- name: a string that can be null
--- The UNIQUE constraint on the email column ensures that no two users can have the same email address.
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
